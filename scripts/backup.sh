92f6cd1854eab8c06dce006fd47d0d5d77fd98fd
David Pecollet <david.pecollet@gmail.com>
2024-10-01 11:18:56 +0100

    removed usage of neo4j admin image

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,1 @@
-
-docker run --interactive --tty --rm \
-    --volume=./backups:/backups \
-    --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    --network=cuenca_db_cluster_net  \
-    neo4j/neo4j-admin:4.4.37-enterprise \
-neo4j-admin backup --backup-dir=/backups --from=neo4j4:6362 --database=neo4j --include-metadata=all
+docker exec --interactive --tty cuenca_db-neo4j4-1 neo4j-admin backup --from=neo4j4:6362 --backup-dir=/backups --database=neo4j --include-metadata=all
--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/env bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-docker run --interactive --tty --rm \
-    --volume=${SCRIPT_DIR}:/scripts \
-    --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    --network=cuenca_db_cluster_net  \
-    neo4j/neo4j-admin:4.4.37-enterprise \
-    cypher-shell -u neo4j -d neo4j -f /scripts/load.cypher -a neo4j://neo4j1:7687
+docker exec --interactive --tty cuenca_db-neo4j1-1 \
+    cypher-shell -u neo4j -d neo4j -f ./scripts/load.cypher -a neo4j://neo4j1:7687
