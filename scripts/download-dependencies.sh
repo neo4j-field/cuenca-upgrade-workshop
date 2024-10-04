4b19ddc8179319e3385139a69127872726055114
David Pecollet <david.pecollet@gmail.com>
2024-10-04 17:43:36 +0100

    added dependencies to download

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -16,3 +16,14 @@ mvn dependency:go-offline
 
 sed -i '' 's/<version>2.7.18<\/version>/<version>3.3.4<\/version>/' pom.xml
 mvn dependency:go-offline
+
+cp "$script_dir"/../plugin/pom.xml .
+mvn dependency:go-offline
+
+sed -i '' 's/<neo4j.version>4.4.37<\/neo4j.version>/<neo4j.version>5.23.0<\/neo4j.version>/' pom.xml
+mvn dependency:go-offline
+
+docker_images=("neo4j:4.4.37-enterprise" "neo4j:5.23.0-enterprise" "eclipse-temurin:17-jre-alpine")
+for image in "${docker_images[@]}"; do
+  docker pull "$image"
+done
